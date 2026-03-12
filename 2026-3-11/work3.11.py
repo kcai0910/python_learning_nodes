49f25391dba3ed41d3832b7ef7afead1fcd33e85
kcai0910 <kcai5725@gmail.com>
2026-03-12 16:42:03 +0800

    Update work3.11.py

--- 2026-3-11/work3.11.py
+++ 2026-3-11/work3.11.py
@@ -5,4 +5,8 @@ height = 1.75
 rid = 5
 sys.set_int_max_str_digits(0)
 info = '===== 个人信息卡 =====\n姓名：%-7s数据类型: %-2222.20s\n年龄: %-3d\t\t数据类型: %s \n身高: %0.2f\t数据类型: %s\n出生年份:%d\n半径为%d的圆的面积:%.2f'%(name,type(name),age,type(age),height,type(height),2026-age,rid,3.1415926*rid**2)
+
 print(info.expandtabs(4));
+#user_name#蛇形命名法
+#userName#小驼峰命名法
+#UserName#大驼峰命名法
