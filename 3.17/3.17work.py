a5da7a4b58e6f2dbea5ef166480fb14de5438f74
kcai0910 <kcai5725@gmail.com>
2026-03-19 09:31:47 +0800

    Update 3.17work.py

--- 3.17/3.17work.py
+++ 3.17/3.17work.py
@@ -67,50 +67,175 @@ def main():
         return break
 while True
 """
-dict1 = {}
-def add():
-    stu1 = input('学生姓名:')
-    dict1[stu1] = None
-def del1() :
-    stu1 = input('请输入需要删除的学生姓名:')
-    if stu1 not in dict1:
-        print('学生不存在')
+# dict1 = {}
+# def add():
+#     stu1 = input('学生姓名:')
+#     dict1[stu1] = None
+# def del1() :
+#     stu1 = input('请输入需要删除的学生姓名:')
+#     if stu1 not in dict1:
+#         print('学生不存在')
+#     else :
+#         dict1.pop(stu1)
+# def check():
+#     print(dict1.items())
+# def infor():
+#     stu1 = input('学生姓名:')
+#     if stu1 in dict1:
+#         cro = input(f'请输入{stu1}的成绩:')
+#         dict1[stu1] = cro
+#     else:
+#           print('学生不存在')
+# def main():
+#     print('\n=========学生管理系统==========')
+#     print('请选择功能\n1:添加学生 2:删除学生 3:查看所有学生 4:录入成绩 5:退出')
+#     res = int(input('请输入你的选择:'))
+#     if res == 1:
+#         add()
+#     elif res == 2:
+#         del1()
+#     elif res == 3:
+#         check()
+#     elif res == 4:
+#         infor()
+#     elif res == 5:
+#         return True
+#     else:
+#         print('输入有误:')
+#         return False
+# while True:
+#     if not main():
+#         n = int(input('是否继续:1:yes 2:no:'))
+#         while   n != 1 and n != 2:
+#             n = int(input('输入错误请重新输入:'))
+#         if n == 1:
+#             continue
+#         elif n == 2:
+#             break
+#     else:
+#         break
+
+#定义一个学生数据容器
+students = []
+def add_stu():
+    print('😀添加学生')
+    stu_id = input('请输入你的学号:')
+    #学号是否重复
+    for stu in students:
+        if stu['id'] == stu_id:
+            print('学号已存在，添加失败!❌')
+            return
+    stu_name = input('请输入你的姓名')
+    students.append({'id': stu_id, 'name': stu_name,'score':''})
+
+def del_stu():
+    print('删除学生')
+    if not students:
+        print('暂无学生信息')
+        return
+    stu_id = input('请输入你要删除的学号:')
+    #声名一个变量用于保存索引值
+    index = ''
+    for i in range (len(students)):
+        if students[i]['id'] == stu_id:
+            index = i
+            break
+    if index != '':
+        students.pop(index)
+        print('删除成功')
     else :
-        dict1.pop(stu1)
-def check():
-    print(dict1.items())
-def infor():
-    stu1 = input('学生姓名:')
-    if stu1 in dict1:
-        cro = input(f'请输入{stu1}的成绩:')
-        dict1[stu1] = cro
-    else:
-          print('学生不存在')
+        print('未找到学生❌')
+
+def show_stu():
+    print('查看所有学生')
+    if not  students:
+        print('暂无学生信息')
+        return
+    print('学号     姓名     成绩')
+    for stu in students:
+    #<左对齐 >右对齐 ^居中
+        print(f'{stu['id']:<8}{stu['name']:<8}{stu['score']:<8}')
+
+def score_stu():
+    print('录入成绩')
+    if not students:
+        print('暂无学生信息')
+        return
+    stu_id = input('请输入你要录取成绩的学号:')
+    for stu in students:
+        if stu['id'] == stu_id:
+            score = input('请输入成绩:')
+            stu['score'] = score
+            print('成绩已成功录入')
+            return
+    print('没有找到该学生😓')
+
 def main():
-    print('\n=========学生管理系统==========')
-    print('请选择功能\n1:添加学生 2:删除学生 3:查看所有学生 4:录入成绩 5:退出')
-    res = int(input('请输入你的选择:'))
-    if res == 1:
-        add()
-    elif res == 2:
-        del1()
-    elif res == 3:
-        check()
-    elif res == 4:
-        infor()
-    elif res == 5:
+    print('===========学生管理=========')
+    print('1:添加学生 2:删除学生 3:查看所有学生 4:录入成绩 5:退出')
+    #用户输入指定操作
+    res = input('请输入你的操作:')
+    # if res not in ('1', '2', '3', '4', '5'):
+    #     print('❌输入有问题,请重新输入 ')
+    if res == '1':
+       add_stu()
+    elif res == '2':
+        del_stu()
+    elif res == '3':
+        show_stu()
+    elif res == '4':
+        score_stu()
+    elif res == '5':
+        print('退出')
         return True
     else:
-        print('输入有误:')
-        return False
+        print('输入错误，请重新输入')
 while True:
-    if not main():
-        n = int(input('是否继续:1:yes 2:no:'))
-        while   n != 1 and n != 2:
-            n = int(input('输入错误请重新输入:'))
-        if n == 1:
-            continue
-        elif n == 2:
-            break
-    else:
+    result = main()
+    if result :
         break
+
+# def add_stu():
+#     print('添加学生😀')
+#     stu_id = input('请输入你的学号:')
+#     for stu in students:
+#         if stu['id'] == stu_id:
+#             return
+#     stu_name = input('请输入你的姓名:')
+#     students.append({'id': stu_id, 'name': stu_name,'score':''})
+#     def show_stu():
+#         if not students:
+#             print('暂无学生信息')
+#             return
+#         print('学号    姓名    成绩')
+#         for stu in students:
+#             print(f'{stu["id"]:<8}{stu["name"]:<8}{stu["score"]:<8}')
+
+# def del_stu():
+#     if not students:
+#         print('暂无学生信息')
+#         return
+#     index = ''
+#     stu_id = input('请输入要删除的学号')
+#     for stu in range(len(students)):
+#         if students[stu]['id'] == stu_id:
+#             index = stu
+#             break
+#     if index != '':
+#         students.pop(index)
+#         print('删除成功')
+#     else :
+#         print('未找到学生❌')
+
+
+# def score_stu():
+#     if not students:
+#         print('暂无学生信息')
+#         return
+#     stu_id = input('请输入你要录取成绩的学号:')
+#     for stu in students:
+#         if stu['id'] == stu_id:
+#             score = input('请输入成绩:')
+#             stu['score'] = score
+#             return
+#     print('没有找到该学生😓')
